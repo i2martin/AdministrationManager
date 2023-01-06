b082690c583f58c6745c4181b1d7c3f82f14d71c
Ivan Martinovic <makz66@hotmail.com>
2023-01-06 16:35:36 +0100

    Added submit to the form and basic functionality to collect and work with input data.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField
@@ -20,23 +20,26 @@ def home():
     return render_template("index.html")
 # Press the green button in the gutter to run the script.
 
+forms = []
 
-@app.route("/putni")
+@app.route("/putni", methods=["GET", "POST"])
 def putni_troskovi():
-    workdays = []
-    year = datetime.now().year
-    month = datetime.now().month
-    for d in wd.get_workdays(year, month, ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']):
-        workdays.append(d.strftime("%d.%m.%Y"))
-    workdays = wd.order_days(workdays)
-    number_of_workdays = len(workdays)
-    print(number_of_workdays)
-    print(workdays)
-    forms = []
-    for i in range(0, number_of_workdays):
-        form = PutniTroskovi()
-        forms.append([form, workdays[i]])
-    return render_template("putni.html", forms=forms, workdays=workdays)
+    if request.method == "POST":
+        print(request.form)
+        return "<h1> Done! </h1>"
+    else:
+        workdays = []
+        year = datetime.now().year
+        month = datetime.now().month
+        for d in wd.get_workdays(year, month, ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']):
+            workdays.append(d.strftime("%d.%m.%Y"))
+        workdays = wd.order_days(workdays)
+        number_of_workdays = len(workdays)
+        forms = []
+        for i in range(0, number_of_workdays):
+            form = PutniTroskovi()
+            forms.append([form, workdays[i]])
+        return render_template("putni.html", forms=forms, workdays=workdays)
 
 
 if __name__ == '__main__':
