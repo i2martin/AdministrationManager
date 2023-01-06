80573dfa98981db3cfdbf115c582cd5463b68fda
Ivan Martinovic <makz66@hotmail.com>
2023-01-06 13:06:45 +0100

    Created new route for "putni_troskovi" and generated a list of workdays for current year/month to be passed on.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-
+import workdays as wd
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -8,9 +10,17 @@ Bootstrap(app)
 def home():
     return render_template("index.html")
 # Press the green button in the gutter to run the script.
+
+
+@app.route("/putni")
+def putni_troskovi():
+    workdays = []
+    year = datetime.now().year
+    month = datetime.now().month
+    for d in wd.get_workdays(year, month, ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']):
+        workdays.append(d.strftime("%d-%m-%Y"))
+    workdays = wd.order_days(workdays)
+
+
 if __name__ == '__main__':
     app.run()
-
-
-
-
